ea55f856eb8bf493db35a9c9285595c8c2c6e061
Alain Paulin Niyonkuru <alainpaulinn@gmail.com>
2022-05-22 00:15:12 +0200

    setup electron app for Imperiumline

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
@@ -24,7 +24,6 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
